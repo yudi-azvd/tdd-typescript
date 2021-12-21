81db718733329ea13c1a084b2861a3d4d6e63800
Yudi Yamane <yudiyamane@gmail.com>
2021-12-21 16:04:51 -0300

    ensure CheckLastEventStatus returns status done when group has no event

--- tests/check-last-event-status.spec.ts
+++ tests/check-last-event-status.spec.ts
@@ -3,8 +3,9 @@ class CheckLastEventStatus {
     private readonly loadLastRepository: LoadLastEventRepository
   ) { }
 
-  async perform(groupId: string): Promise<void> {
+  async perform(groupId: string): Promise<string> {
     await this.loadLastRepository.loadLastEvent(groupId)
+    return 'done'
   }
 }
 
@@ -12,20 +13,35 @@ interface LoadLastEventRepository  {
   loadLastEvent(groupId: string): Promise<void>
 }
 
-class LoadLastEventRepositoryMock implements LoadLastEventRepository {
-  groupId?: string;
+class LoadLastEventRepositorySpy implements LoadLastEventRepository {
+  groupId?: string
+  callsCount = 0
+  output: undefined
 
   async loadLastEvent(groupId: string): Promise<void> {
     this.groupId = groupId
+    this.callsCount++
+    return this.output
   }
 }
 
 describe('CheckLastEventStatus', () => {
   test('should get last event data', async () => {
-    const loadLastEventRepository = new LoadLastEventRepositoryMock();
-    const checkLastEventStatus = new CheckLastEventStatus(loadLastEventRepository);
+    const loadLastEventRepository = new LoadLastEventRepositorySpy()
+    const sut = new CheckLastEventStatus(loadLastEventRepository)
 
-    await checkLastEventStatus.perform('any_group_id');
-    expect(loadLastEventRepository.groupId).toEqual('any_group_id');
-  });
-});
+    await sut.perform('any_group_id')
+    expect(loadLastEventRepository.groupId).toEqual('any_group_id')
+    expect(loadLastEventRepository.callsCount).toEqual(1)
+  })
+
+  test('should return status done when group has no events', async () => {
+    const loadLastEventRepository = new LoadLastEventRepositorySpy()
+    loadLastEventRepository.output = undefined
+    const sut = new CheckLastEventStatus(loadLastEventRepository)
+
+    const status = await sut.perform('any_group_id')
+
+    expect(status).toBe('done')
+  })
+})
