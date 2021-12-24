04b20667b5156a6601eb9627fd2fccab4df366ca
Yudi Yamane <yudiyamane@gmail.com>
2021-12-24 17:34:12 -0300

    ensure DeleteEvent calls DeleteMatchRepository correctly

--- tests/delete-event.spec.ts
+++ tests/delete-event.spec.ts
@@ -1,6 +1,8 @@
 class DeleteEvent {
   constructor(
-    private readonly loadGroupRepository: LoadGroupRepository
+    private readonly loadGroupRepository: LoadGroupRepository,
+    private readonly deleteEventRepository: DeleteEventRepository,
+    private readonly deleteMatchRepository: DeleteMatchRepository,
   ) { }
 
   async perform({ id, userId }: { id: string, userId: string }): Promise<void> {
@@ -11,12 +13,15 @@ class DeleteEvent {
       throw new Error()
     if (group.users.find(user => user.id === userId)?.permission === 'user')
       throw new Error()
+
+    await this.deleteEventRepository.delete({ id })
+    await this.deleteMatchRepository.delete({ eventId: id })
   }
 }
 
 type GroupUser = {
   id: string
-  permission: string
+  permission: 'owner' | 'admin' | 'user'
 }
 
 type Group = {
@@ -28,10 +33,34 @@ interface LoadGroupRepository {
   load(input: { eventId: string }): Promise<Group | undefined>
 }
 
-class LoadGroupRepositoryMock implements LoadGroupRepository {
+interface DeleteEventRepository {
+  delete(input: { id: string }): Promise<void>
+}
+
+interface DeleteMatchRepository {
+  delete(input: { eventId: string }): Promise<void>
+}
+
+class DeleteEventRepositorySpy implements DeleteEventRepository {
+  id?: string
+
+  async delete({ id }: { id: string }): Promise<void> {
+    this.id = id
+  }
+}
+
+class DeleteMatchRepositorySpy implements DeleteMatchRepository {
+  eventId?: string
+
+  async delete({ eventId }: { eventId: string }): Promise<void> {
+    this.eventId = eventId
+  }
+}
+
+class LoadGroupRepositorySpy implements LoadGroupRepository {
   eventId?: string
   output?: Group = {
-    users: [{ id: 'any_user_id', permission: 'any' }]
+    users: [{ id: 'any_user_id', permission: 'admin' }]
   }
 
   async load({ eventId }: { eventId: string }): Promise<Group | undefined> {
@@ -42,13 +71,22 @@ class LoadGroupRepositoryMock implements LoadGroupRepository {
 
 type SutTypes = {
   sut: DeleteEvent,
-  loadGroupRepository: LoadGroupRepositoryMock
+  loadGroupRepository: LoadGroupRepositorySpy,
+  deleteEventRepository: DeleteEventRepositorySpy,
+  deleteMatchRepository: DeleteMatchRepositorySpy,
 }
 
 const makeSut = (): SutTypes => {
-  const loadGroupRepository = new LoadGroupRepositoryMock()
-  const sut = new DeleteEvent(loadGroupRepository)
-  return { sut, loadGroupRepository }
+  const loadGroupRepository = new LoadGroupRepositorySpy()
+  const deleteEventRepository = new DeleteEventRepositorySpy()
+  const deleteMatchRepository = new DeleteMatchRepositorySpy()
+  const sut = new DeleteEvent(loadGroupRepository, deleteEventRepository, deleteMatchRepository)
+  return {
+    sut,
+    loadGroupRepository,
+    deleteEventRepository,
+    deleteMatchRepository
+  }
 }
 
 describe('DeleteEvent', () => {
@@ -75,7 +113,7 @@ describe('DeleteEvent', () => {
   it('should throw exception if eventId is invalid', async () => {
     const { sut, loadGroupRepository } = makeSut()
     loadGroupRepository.output = {
-      users: [{ id: 'any_user_id', permission: 'any' }]
+      users: [{ id: 'any_user_id', permission: 'admin' }]
     }
 
     const promise = sut.perform({ id, userId: 'invalid-id' })
@@ -92,6 +130,44 @@ describe('DeleteEvent', () => {
     const promise = sut.perform({ id, userId })
 
     await expect(promise).rejects.toThrowError()
+  })
+
+  it('should not throw if permission is admin', async () => {
+    const { sut, loadGroupRepository } = makeSut()
+    loadGroupRepository.output = {
+      users: [{ id: 'any_user_id', permission: 'admin' }]
+    }
+
+    const promise = sut.perform({ id, userId })
+
+    await expect(promise).resolves.not.toThrowError()
+  })
+
+  it('should not throw if permission is owner', async () => {
+    const { sut, loadGroupRepository } = makeSut()
+    loadGroupRepository.output = {
+      users: [{ id: 'any_user_id', permission: 'owner' }]
+    }
+
+    const promise = sut.perform({ id, userId })
+
+    await expect(promise).resolves.not.toThrowError()
+  })
+
+  it('should delete event', async () => {
+    const { sut, deleteEventRepository } = makeSut()
+
+    await sut.perform({ id, userId })
+
+    expect(deleteEventRepository.id).toBe(id)
+  })
+
+  it('should delete matches', async () => {
+    const { sut, deleteMatchRepository } = makeSut()
+
+    await sut.perform({ id, userId })
+
+    expect(deleteMatchRepository.eventId).toBe(id)
   })
 
 })
