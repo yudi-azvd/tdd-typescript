7ac192e742ff3299474fb44d10739b026c60843c
Yudi Yamane <yudiyamane@gmail.com>
2021-12-24 09:26:51 -0300

    enure DeleteEvent throws if eventId is invalid

--- tests/delete-event.spec.ts
+++ tests/delete-event.spec.ts
@@ -4,32 +4,55 @@ class DeleteEvent {
   ) { }
 
   async perform({ id }: { id: string, userId: string }): Promise<void> {
-    await this.loadGroupRepository.load({ eventId: id })
+    const group = await this.loadGroupRepository.load({ eventId: id })
+    if (group === undefined) throw new Error()
   }
 }
 
 interface LoadGroupRepository {
   eventId?: string
-  load(input: { eventId: string }): Promise<void>
+  load(input: { eventId: string }): Promise<any>
 }
 
 class LoadGroupRepositoryMock implements LoadGroupRepository {
   eventId?: string
+  output: any = 'any_value'
 
-  async load({ eventId }: { eventId: string }): Promise<void> {
+  async load({ eventId }: { eventId: string }): Promise<any> {
     this.eventId = eventId
+    return this.output
   }
 }
 
+type SutTypes = {
+  sut: DeleteEvent,
+  loadGroupRepository: LoadGroupRepositoryMock
+}
+
+const makeSut = (): SutTypes => {
+  const loadGroupRepository = new LoadGroupRepositoryMock()
+  const sut = new DeleteEvent(loadGroupRepository)
+  return { sut, loadGroupRepository }
+}
+
 describe('DeleteEvent', () => {
+  const id = 'any_event_id'
+  const userId = 'any_user_id'
+
   it('should get last event data', async () => {
-    const id = 'any_event_id'
-    const userId = 'any_user_id'
-    const loadGroupRepository = new LoadGroupRepositoryMock()
-    const sut = new DeleteEvent(loadGroupRepository)
+    const { sut, loadGroupRepository } = makeSut()
 
     await sut.perform({ id, userId })
 
     expect(loadGroupRepository.eventId).toBe(id)
+  })
+
+  it('should throw exception if eventId is invalid', async () => {
+    const { sut, loadGroupRepository } = makeSut()
+    loadGroupRepository.output = undefined
+
+    const promise = sut.perform({ id, userId })
+
+    expect(promise).rejects.toThrowError()
   })
 })
